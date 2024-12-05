a19cd2c63ec5fe784ca4e740c13d5bd2bb7278d2
all-rounder <xiaoyao16@gmail.com>
2024-12-04 22:22:29 -0500

    error handling

--- EmotionDetection/emotion_detection.py
+++ EmotionDetection/emotion_detection.py
@@ -19,10 +19,14 @@ def emotion_detector(text_to_analyze):
     headers = {"grpc-metadata-mm-model-id": "emotion_aggregated-workflow_lang_en_stock"}
     myobj = { "raw_document": { "text": text_to_analyze } }
     response = requests.post(url, json = myobj, headers = headers)
-    formatted_response = json.loads(response.text)
 
-    emotion_rating = formatted_response["emotionPredictions"][0]["emotion"]
-    emotion_rating["dominant_emotion"] = dominant_emotion(emotion_rating)
+    if response.status_code == 200:
+        formatted_response = json.loads(response.text)
+        emotion_rating = formatted_response["emotionPredictions"][0]["emotion"]
+        emotion_rating["dominant_emotion"] = dominant_emotion(emotion_rating)
+    elif response.status_code == 400:
+        emotion_rating = {"anger": None, "disgust": None, "fear": None, "joy": None, "sadness": None, "dominant_emotion": None}
+
     return emotion_rating
 
 
