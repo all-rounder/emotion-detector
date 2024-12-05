abc71af92ba7277c79a9974087879155a73e9e0c
all-rounder <xiaoyao16@gmail.com>
2024-12-04 22:44:13 -0500

    static code analysis

--- server.py
+++ server.py
@@ -1,11 +1,13 @@
+"""Module providing a function printing python version."""
+
 from flask import Flask, render_template, request
 from EmotionDetection.emotion_detection import emotion_detector
 
 app = Flask("Emotion Detector")
 
-
 @app.route('/emotionDetector')
 def emo_detector():
+    """Function detect emotion"""
     # Retrieve the text to analyze from the request arguments
     text_to_analyze = request.args.get('textToAnalyze')
     # Pass the text to the emotion_detector function and store the response
@@ -13,17 +15,22 @@ def emo_detector():
 
     dominant_emotion = response["dominant_emotion"]
     if dominant_emotion is None:
-        return "Invalid text! Please try again!"
+        output = "Invalid text! Please try again!"
     else:
     # Return a formatted string
-        output = f"For the given statement, the system response is 'anger': {response['anger']}, 'disgust': {response['disgust']}, 'fear': {response['fear']}, 'joy': {response['joy']} and 'sadness': {response['sadness']}. The dominant emotion is joy."
-        return output
+        output = f"""For the given statement, the system response is 'anger': {response['anger']},
+        'disgust': {response['disgust']}, 'fear': {response['fear']},
+        'joy': {response['joy']} and 'sadness': {response['sadness']}.
+        The dominant emotion is joy."""
+
+    return output
 
 
 @app.route("/")
 def render_index_page():
+    """Function render index page"""
     return render_template('index.html')
 
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000)    
+    app.run(host="0.0.0.0", port=5000)
